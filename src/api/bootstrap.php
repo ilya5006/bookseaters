f4a9a6fe1fbfe8e6a2611df636ebee80f918efee
ilya5006 <ilya.samarczew2011@yandex.ru>
2022-09-01 15:39:47 +0300

    Bootstrap carcass

--- src/api/bootstrap.php
+++ src/api/bootstrap.php
@@ -1,3 +1,10 @@
 <?php
 
 require_once __DIR__ . '/../config.php';
+require_once __DIR__ . '/../model/RouteParser.php';
+require_once __DIR__ . '/../model/Database.php';
+
+$db = new Database(DB_HOST, DB_LOGIN, DB_PASSWORD, DB_NAME);
+$routeParser = new RouteParser(
+    rtrim($_SERVER['REQUEST_URI'], '/')
+);
