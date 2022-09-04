5026875964a62148c5ae0ae9b7d5723092cf4c41
ilya5006 <ilya.samarczew2011@yandex.ru>
2022-09-04 13:44:48 +0300

    Database connection close

--- src/model/Database.php
+++ src/model/Database.php
@@ -10,13 +10,20 @@ class Database {
         private string $dbName,
     ) {
         $this->connection = new mysqli($dbHost, $dbLogin, $dbPassword, $dbName);
+
+        if ($this->connection->connect_error) {
+        }
     }
 
     public function query(string $query) {
         $queryResult = $this->connection->query(
-            $this->connection->escape_string($query),
+            $this->connection->escape_string($query)
         );
 
         return $queryResult->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function __destruct() {
+        $this->connection->close();
     }
 }
