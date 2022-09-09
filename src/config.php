ac0aa89b8e939b3e0af93f6ad52f514c2a5851eb
ilya5006 <ilya.samarczew2011@yandex.ru>
2022-09-09 15:47:22 +0300

    Logs paths in config

--- src/config.php
+++ src/config.php
@@ -4,3 +4,6 @@ define('DB_HOST', 'localhost');
 define('DB_LOGIN', 'root');
 define('DB_PASSWORD', 'root');
 define('DB_NAME', 'bookseaters');
+
+define('API_LOGS_DIR', __DIR__ . '/logs/api/');
+define('SITE_LOGS_DIR', __DIR__ . '/logs/site/');
